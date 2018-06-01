5cb845a947a4cd1cc5bd3f4b159dd989e5c31c53
HudaSalah <hudasalah29@gmail.com>
2018-06-02 00:01:53 +0200

    upload flower

---
+++ flower.js
@@ -0,0 +1,124 @@
+
+$(document).ready(function(){
+   
+    
+$(".img-circle").hover(function(e){
+    
+    $(this).css("transform", "rotate(40deg)");
+    
+   $(".p1").css({
+       
+       position:"absolute",
+	   top:"10%",
+	   left:"5%"
+       
+   }); 
+    
+    $(".p2").css({
+        
+        position:"absolute",
+	    top:"39%",
+	    left:"3%"
+        
+    });
+    
+    
+     $(".p3").css({
+        
+        position:"absolute",
+	    top:"64%",
+	    left:"4%"
+        
+    });
+    
+    
+     $(".p5").css({
+        
+        position:"absolute",
+	    top:"10%",
+	    left:"75%"
+        
+    });
+    
+    
+     $(".p6").css({
+        
+        position:"absolute",
+	    left:"76%"
+        
+    });
+    
+     $(".p7").css({
+        
+        position:"absolute",
+	    top:"65%",
+	    left:"77%"
+        
+    });
+    
+    
+} , 
+  function(e){
+    
+     $(this).css("transform", "rotate(20deg)");
+     $(".p1").css({
+       
+       position:"absolute",
+	   top:"17%",
+	   left:"16%"
+       
+   }); 
+    
+    $(".p2").css({
+        
+        position:"absolute",
+	    top:"40%",
+	    left:"15.5%"
+        
+    });
+    
+    
+     $(".p3").css({
+        
+        position:"absolute",
+	    top:"60%",
+	    left:"18%"
+        
+    });
+    
+    
+     $(".p5").css({
+        
+        position:"absolute",
+	    top:"19%",
+	    left:"63.7%"
+        
+    });
+    
+    
+     $(".p6").css({
+        
+        position:"absolute",
+         top:"39%",
+	    left:"66%"
+        
+    });
+    
+     $(".p7").css({
+        
+        position:"absolute",
+	    top:"61%",
+	    left:"63.5%"
+        
+    });
+    
+    
+         }
+                      
+                      
+                      
+    );
+    
+    
+});
+
